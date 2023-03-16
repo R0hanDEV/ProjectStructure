c0357075bef220ebd98d8d5359998dfdf596776c
rohan <r0han45dev@gmail.com>
2023-03-16 11:51:08 +0530

    add fonts in react project and create a form component

--- Reactjs/reactjs-mvc-project/src/App.js
+++ Reactjs/reactjs-mvc-project/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import InputForm from './pages/form/inputForm';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' Component={InputForm} />
+          <Route path='/login' Component={InputForm} />
+          <Route path='/signup' Component={InputForm} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ Reactjs/reactjs-mvc-project/src/routes/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+export default function ListOfRoutes() {
+    return (
+        <Router>
+            <Routes>
+                <Route element></Route>
+            </Routes>
+        </Router>
+    )
+}
+
