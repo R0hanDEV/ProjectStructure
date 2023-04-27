0db8dc155c12fea6d51cba99291671b2c10c2d5e
Rohan Vishwakarma <rohan.vishwakarma@think360.ai>
2023-04-27 15:06:24 +0530

    404 not found logic added with cors setting.

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -3,6 +3,7 @@ const path = require("path")
 const express = require("express")
 const dotenv = require("dotenv")
 const cookiesParser = require("cookie-parser")
+const bodyParser = require('body-parser')
 
 
 /* import security packages to avoid injection hit to your system */
@@ -19,13 +20,26 @@ const app = express()
 
 
 /* invoke cors and securities to app */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors())
 app.use(mongoSanitize())
 
+/* cors setting */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'POST, GET, PATCH, DELETE, OPTIONS'
+    );
+    next();
+})
 
 /* invoke middlewarees */
-const { Notfoundroutes } = require("./middleware/notfoundroutes")
-
 app.use(express.json());
 app.use(cookiesParser());
 
@@ -38,7 +52,11 @@ const getRoutes = require("./routes/users")
 /* invokes routes to app */
 app.use("/api/v1/", getRoutes);
 
-app.use("*", Notfoundroutes)
+
+// not found
+app.use((req, res) => {
+    res.status(404).send({ code: 404, status: "fail", data: { error: `${req.originalUrl} not found` } });
+});
 
 
 /* module export app */
